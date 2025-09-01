1fe78eff4ee9b72510b35f72667e6f8e4c49b587
Usman Hasan <usmanhasan197@gmail.com>
2025-09-01 19:36:53 +0500

    Update Q7.cpp

--- Lab 2/Q7.cpp
+++ Lab 2/Q7.cpp
@@ -2,59 +2,55 @@
 using namespace std;
 
 int main() {
-    int departments;
-    cout << "Enter number of departments: ";
-    cin >> departments;
-
-    int* empCount = new int[departments];
-    int** salaries = new int*[departments];
-
-    for (int i = 0; i < departments; i++) {
-        cout << "Enter number of employees in Department " << i + 1 << ": ";
-        cin >> empCount[i];
-        salaries[i] = new int[empCount[i]];
-
-        cout << "Enter salaries for Department " << i + 1 << ":" << endl;
-        for (int j = 0; j < empCount[i]; j++) {
-            cout << "Employee " << j + 1 << ": ";
-            cin >> salaries[i][j];
-        }
-    }
-
-    cout << endl << "Highest salary in each department " << endl;
-    for (int i = 0; i < departments; i++) {
-        int maxSalary = salaries[i][0];
-        for (int j = 1; j < empCount[i]; j++) {
-            if (salaries[i][j] > maxSalary) {
-                maxSalary = salaries[i][j];
-            }
-        }
-        cout << "Department " << i + 1 << ": " << maxSalary << endl;
-    }
-
-    int bestDept = 0;
-    double maxAvg = 0;
-
-    for (int i = 0; i < departments; i++) {
-        int sum = 0;
-        for (int j = 0; j < empCount[i]; j++) {
-            sum += salaries[i][j];
-        }
-        double avg = static_cast<double>(sum) / empCount[i];
-        if (avg > maxAvg) {
-            maxAvg = avg;
-            bestDept = i;
-        }
-    }
-
-    cout << endl << "Department with maximum average salary: Department "
-         << bestDept + 1 << " (Average = " << maxAvg << ")" << endl;
-
-    for (int i = 0; i < departments; i++) {
-        delete[] salaries[i];
-    }
-    delete[] salaries;
-    delete[] empCount;
-
-    return 0;
+	int n;
+	cout<<"enter number of departments: ";
+	cin>>n;
+	
+	int *emp=new int[n];
+	int **sal=new int*[n];
+	
+	for(int i=0;i<n;i++){
+		cout<<"enter number of employees in dept "<<i+1<<": ";
+		cin>>emp[i];
+		sal[i]=new int[emp[i]];
+		
+		cout<<"enter salaries for dept "<<i+1<<":"<<endl;
+		for(int j=0;j<emp[i];j++){
+			cout<<"employee "<<j+1<<": ";
+			cin>>sal[i][j];
+		}
+	}
+	
+	cout<<"highest salary in each dept"<<endl;
+	for(int i=0;i<n;i++){
+		int mx=sal[i][0];
+		for(int j=1;j<emp[i];j++){
+			if(sal[i][j]>mx){
+				mx=sal[i][j];
+			}
+		}
+		cout<<"dept "<<i+1<<": "<<mx<<endl;
+	}
+	
+	int best=0;
+	double mxavg=0;
+	for(int i=0;i<n;i++){
+		int sum=0;
+		for(int j=0;j<emp[i];j++){
+			sum+=sal[i][j];
+		}
+		double avg=(double)sum/emp[i];
+		if(avg>mxavg){
+			mxavg=avg;
+			best=i;
+		}
+	}
+	
+	cout<<"dept with max avg salary: dept "<<best+1<<" (avg = "<<mxavg<<")"<<endl;
+	
+	for(int i=0;i<n;i++){
+		delete[] sal[i];
+	}
+	delete[] sal;
+	delete[] emp;
 }
