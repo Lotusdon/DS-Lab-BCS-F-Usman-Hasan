c19336a4ffa32b52773abe1814e07442af3f89bb
Usman Hasan <usmanhasan197@gmail.com>
2025-09-01 19:35:23 +0500

    Update Q5.cpp

--- Lab 2/Q5.cpp
+++ Lab 2/Q5.cpp
@@ -2,82 +2,88 @@
 using namespace std;
 
 int main() {
-    int rows, cols;
-    cout << "Enter number of rows: ";
-    cin >> rows;
-    cout << "Enter number of columns: ";
-    cin >> cols;
-
-    double** A = new double*[rows];
-    double** B = new double*[rows];
-    double** Add = new double*[rows];
-    double** Sub = new double*[rows];
-
-    for (int i = 0; i < rows; i++) {
-        A[i] = new double[cols];
-        B[i] = new double[cols];
-        Add[i] = new double[cols];
-        Sub[i] = new double[cols];
-    }
-
-    cout << endl << "Enter elements of Matrix A:" << endl;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << "A[" << i << "][" << j << "]: ";
-            cin >> A[i][j];
-        }
-    }
-
-    cout << endl << "Enter elements of Matrix B:" << endl;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << "B[" << i << "][" << j << "]: ";
-            cin >> B[i][j];
-        }
-    }
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            Add[i][j] = A[i][j] + B[i][j];
-            Sub[i][j] = A[i][j] - B[i][j];
-        }
-    }
-
-    cout << endl << "Matrix A:" << endl;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) cout << A[i][j] << " ";
-        cout << endl;
-    }
-
-    cout << endl << "Matrix B:" << endl;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) cout << B[i][j] << " ";
-        cout << endl;
-    }
-
-    cout << endl << "Addition (A + B):" << endl;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) cout << Add[i][j] << " ";
-        cout << endl;
-    }
-
-    cout << endl << "Subtraction (A - B):" << endl;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) cout << Sub[i][j] << " ";
-        cout << endl;
-    }
-
-    for (int i = 0; i < rows; i++) {
-        delete[] A[i];
-        delete[] B[i];
-        delete[] Add[i];
-        delete[] Sub[i];
-    }
-
-    delete[] A;
-    delete[] B;
-    delete[] Add;
-    delete[] Sub;
-
-    return 0;
+	int row,col;
+	cout<<"enter number of rows: ";
+	cin>>row;
+	cout<<"enter number of columns: ";
+	cin>>col;
+	
+	double **a=new double*[row];
+	double **b=new double*[row];
+	double **add=new double*[row];
+	double **sub=new double*[row];
+	
+	for(int i=0;i<row;i++){
+		a[i]=new double[col];
+		b[i]=new double[col];
+		add[i]=new double[col];
+		sub[i]=new double[col];
+	}
+	
+	cout<<"enter elements of matrix a:"<<endl;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<"a["<<i<<"]["<<j<<"]: ";
+			cin>>a[i][j];
+		}
+	}
+	
+	cout<<"enter elements of matrix b:"<<endl;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<"b["<<i<<"]["<<j<<"]: ";
+			cin>>b[i][j];
+		}
+	}
+	
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			add[i][j]=a[i][j]+b[i][j];
+			sub[i][j]=a[i][j]-b[i][j];
+		}
+	}
+	
+	cout<<"matrix a:"<<endl;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	cout<<"matrix b:"<<endl;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<b[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	cout<<"addition (a+b):"<<endl;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<add[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	cout<<"subtraction (a-b):"<<endl;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<sub[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	for(int i=0;i<row;i++){
+		delete[] a[i];
+		delete[] b[i];
+		delete[] add[i];
+		delete[] sub[i];
+	}
+	
+	delete[] a;
+	delete[] b;
+	delete[] add;
+	delete[] sub;
 }
