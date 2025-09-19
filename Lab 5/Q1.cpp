3d7b60254cc35a7ecda34f4fc1858758ccbc6958
Usman Hasan <usmanhasan197@gmail.com>
2025-09-19 11:24:00 +0500

    Create Q1.cpp

---
+++ Lab 5/Q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+	if (n==0) {
+		return 1;
+	}
+	else {
+		return n*fact(n-1);
+	}
+}
+int main() {
+	int n;
+	cout<<"enter number: ";
+	cin>>n;
+	cout<<"factorial of number is "<<fact(n)<<endl;
+}
