e4c6436b457e8f81e46cd612064f4405c7db096a
Usman Hasan <usmanhasan197@gmail.com>
2025-09-19 11:59:32 +0500

    Create Q4.cpp

---
+++ Lab 5/Q4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int ackerman(int m,int n) {
+	if (m==0) {
+		
+		return n+1;
+	}
+	else if (m<0 && n==0) {
+		return ackerman(m-1,1);
+		
+
+	}
+	else if (m>0 && n>0) {
+		return ackerman(m-1,ackerman(m,n-1));
+	}
+}
+
+
+int main() {
+	int m,n;
+	cout<<"enter number m: ";
+	cin>>n;
+	cout<<"enter number n: ";
+	cin>>n;
+	cout<<"output:"<<ackerman(m,n);
+}
