145c10011ed91f83326c5b3fb50aa0ba4cc1e113
Usman Hasan <usmanhasan197@gmail.com>
2025-10-17 12:11:05 +0500

    Update Q1.cpp

--- Lab 6/Q1.cpp
+++ Lab 6/Q1.cpp
@@ -1,73 +1,73 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_SIZE 100
+#define max_size 100
 
-class Stack {
+class stack {
 private:
-    int arr[MAX_SIZE];
-    int top;
+ int arr[max_size];
+ int top;
 
 public:
-    Stack() {
-        top = -1;
-    }
+ stack() {
+  top = -1;
+ }
 
-    void push(int element) {
-        if (top >= MAX_SIZE - 1) {
-            cout << "Stack overflow! Cannot push " << element << endl;
-            return;
-        }
-        arr[++top] = element;
-        cout << "Pushed " << element << " to stack." << endl;
-    }
+ void push(int element) {
+  if (top >= max_size - 1) {
+   cout << "stack overflow! cannot push " << element << endl;
+   return;
+  }
+  arr[++top] = element;
+  cout << "pushed " << element << " to stack." << endl;
+ }
 
-    int pop() {
-        if (top < 0) {
-            cout << "Stack underflow! Cannot pop." << endl;
-            return -1;
-        }
-        int popped = arr[top--];
-        cout << "Popped " << popped << " from stack." << endl;
-        return popped;
-    }
+ int pop() {
+  if (top < 0) {
+   cout << "stack underflow! cannot pop." << endl;
+   return -1;
+  }
+  int popped = arr[top--];
+  cout << "popped " << popped << " from stack." << endl;
+  return popped;
+ }
 
-    int peek() {
-        if (top < 0) {
-            cout << "Stack is empty. Nothing to peek." << endl;
-            return -1;
-        }
-        cout << "Top element is " << arr[top] << endl;
-        return arr[top];
-    }
+ int peek() {
+  if (top < 0) {
+   cout << "stack is empty. nothing to peek." << endl;
+   return -1;
+  }
+  cout << "top element is " << arr[top] << endl;
+  return arr[top];
+ }
 
-    void display() {
-        if (top < 0) {
-            cout << "Stack is empty." << endl;
-            return;
-        }
-        cout << "Stack contents from bottom to top:" << endl;
-        for (int i = 0; i <= top; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
+ void display() {
+  if (top < 0) {
+   cout << "stack is empty." << endl;
+   return;
+  }
+  cout << "stack contents from bottom to top:" << endl;
+  for (int i = 0; i <= top; i++) {
+   cout << arr[i] << " ";
+  }
+  cout << endl;
+ }
 };
 
 int main() {
-    Stack s;
+ stack s;
 
-    s.push(10);
-    s.push(20);
-    s.push(30);
+ s.push(10);
+ s.push(20);
+ s.push(30);
 
-    s.display();
+ s.display();
 
-    s.peek();
+ s.peek();
 
-    s.pop();
+ s.pop();
 
-    s.display();
+ s.display();
 
-    return 0;
+ return 0;
 }
